178c67a2d9119bd3600f42ba05b7aedaa1dbaad3
Sai Chandan Mohanty <saichandan68@gmail.com>
2024-02-02 12:57:29 +0530

    add react-router-dom and add login, signup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { BrowserRouter,Route, Routes } from 'react-router-dom';
 import Form from "./components/Form";
 function App() {
   return (
-    <div className="App">
-      <Form text="Login" />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Form text="Signup" />} />
+        <Route path="/login" element={<Form text="Login" />} />
+        <Route path="/dashboard" element={<Form text="Dashboard" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
