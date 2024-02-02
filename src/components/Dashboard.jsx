3449363e7be0539e217cf25b6be4a642428cda27
Sai Chandan Mohanty <saichandan68@gmail.com>
2024-02-02 15:27:30 +0530

    Add token logging and fetch latest news articles

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,6 +5,7 @@ const Dashboard = () => {
   const [newsArticles, setNewsArticles] = useState([]);
   useEffect(() => {
     const token = localStorage.getItem("token");
+    console.log(token);
     api
       .get("/news-items", {
         headers: { Authorization: `Bearer ${token}` },
@@ -19,13 +20,42 @@ const Dashboard = () => {
       .catch((err) => {
         console.log(err);
       });
-  }, [newsArticles]);
+  }, []);
 
   return (
     <div className="container mx-auto">
-      <div className="flex bg-custom-orange  items-center mb-4">
-        <img src="/y18.svg" alt="Logo" className="w-6 h-6 m-2 border border-white" />
-        <h1 className="text-2xl font-bold ">Hacker News Clone</h1>
+      <div className="flex bg-custom-orange items-center mb-4">
+        <img
+          src="/y18.svg"
+          alt="Logo"
+          className="w-6 h-6 m-2 border border-white"
+        />
+        <h1 className="text-2xl font-bold">Hacker News Clone</h1>
+        <button
+          className="ml-auto border-black border-2 rounded m-2 p-1 font-bold"
+          onClick={() => {
+            api
+              .post(
+                "/news-items",
+                {},
+                {
+                  headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                  },
+                }
+              )
+              .then((res) => {
+                // Trigger useEffect here
+                console.log(res.data);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+            console.log(localStorage.getItem("token"));
+          }}
+        >
+          Fetch latest
+        </button>
       </div>
       <ul className="space-y-2">
         {newsArticles.map((article, idx) => (
